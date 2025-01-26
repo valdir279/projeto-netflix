2f85359d508adff1f4c7ebc805735d530d834422
valdir279 <valdir.c123@gmail.com>
2025-01-26 17:32:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+let botaoSom = document.querySelector(".botao-som")
+let video =document.querySelector(".video")
+botaoSom.addEventListener("click" , ligaSom,)
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+
+
+function ligaSom(){
+   video.muted =false
+}
+
+botao.addEventListener("click")
+
+function mostrarModal() {
+    modal.style.display = "block"
+
+}
